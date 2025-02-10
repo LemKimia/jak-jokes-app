71f01dcec793368bc2c2bff79929bdee8df9eb8a
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-10 21:38:29 +0700

    feat: set up necessary library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { defaultConfig } from "@tamagui/config/v4";
+import { TamaguiProvider, createTamagui } from "@tamagui/core";
+
+const config = createTamagui(defaultConfig);
+
+type Conf = typeof config;
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
+  );
 }
