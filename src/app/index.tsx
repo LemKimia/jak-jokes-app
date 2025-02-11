58079b968a2e307c005ad72174860d9cfb5354bc
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-11 21:48:05 +0700

    feat: implement joke queries using react-query for improved data fetching

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,28 @@
 import apiQuery from "../utils/query";
 import HomeScreen from "../screen/HomeScreen";
+import { useQueries } from "@tanstack/react-query";
+import api from "../utils/api";
 
 const Index = () => {
-  const {
-    errorFetchingCategory,
-    isFetchingJokesCategory,
-    jokesCategory,
-    jokesCategoryFetched,
-  } = apiQuery.fetchJokesCategory();
+  const { isFetchingJokesCategory, jokesCategory } =
+    apiQuery.fetchJokesCategory();
+  const { jokes } = apiQuery.fetchJokes("any");
+  const cleanJokesCategory = jokesCategory.slice(0, 5)
+
+  const jokesQueries = useQueries({
+    queries: (cleanJokesCategory ?? ["any"]).map((category: string) => ({
+      queryKey: ["jokesQuery", category],
+      queryFn: () => api.getJokes(category),
+    })),
+  });
+  const jokesData = jokesQueries.map((query) => query.data || []);
+  const isLoading = jokesQueries.some((query) => query.isLoading);
+
   return (
     <HomeScreen
       isFetchingJokesCategory={isFetchingJokesCategory}
       jokesCategory={jokesCategory}
+
     />
   );
 };
