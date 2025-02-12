58b00b3a68bdf5d4acc8b26ca4db1bd9c3252afe
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-12 11:46:11 +0700

    feat: update HomeScreen to use Spinner for loading state and improve jokes category handling

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -6,6 +6,7 @@ import {
   Paragraph,
   Square,
   Button,
+  Spinner,
 } from "tamagui";
 import { ChevronDown } from "@tamagui/lucide-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -13,7 +14,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Jokes } from "../utils/type";
 
 type HomeScreenProps = {
-  jokes: Jokes[];
+  jokes: Record<string, Jokes[]>;
   jokesCategory: string[];
   handleFetchMoreJokes: (category: string) => void;
   isFetchingJokesCategory: boolean;
@@ -24,7 +25,8 @@ const HomeScreen = ({
   jokes,
   jokesCategory,
   isFetchingJokesCategory,
-  getJokesByCategory,handleFetchMoreJokes
+  getJokesByCategory,
+  handleFetchMoreJokes,
 }: HomeScreenProps) => {
   const { bottom, top } = useSafeAreaInsets();
 
@@ -46,55 +48,60 @@ const HomeScreen = ({
         alignItems="center"
       >
         {isFetchingJokesCategory ? (
-          <Text>Loading...</Text>
+          <Spinner size="large" />
+        ) : jokesCategory.length === 0 ? (
+          <Text>No joke categories available.</Text>
         ) : (
           <Accordion overflow="hidden" width="$20" type="multiple">
-            {jokesCategory.map((category, index) => (
-              <Accordion.Item key={index} value={`joke-${index}`}>
-                <Accordion.Trigger
-                  flexDirection="row"
-                  justifyContent="space-between"
-                >
-                  {({ open }: { open: boolean }) => (
-                    <>
-                      <Paragraph>
-                        {index + 1}. {category}
-                      </Paragraph>
-                      <Square
-                        animation="quick"
-                        rotate={open ? "180deg" : "0deg"}
-                      >
-                        <ChevronDown size="$1" />
-                      </Square>
-                    </>
-                  )}
-                </Accordion.Trigger>
-                <Accordion.HeightAnimator animation="medium">
-                  <Accordion.Content
-                    animation="medium"
-                    exitStyle={{ opacity: 0 }}
+            {jokesCategory.map((category, index) => {
+              const jokesList = getJokesByCategory(category);
+              return (
+                <Accordion.Item key={index} value={`joke-${index}`}>
+                  <Accordion.Trigger
+                    flexDirection="row"
+                    justifyContent="space-between"
                   >
-                    {getJokesByCategory(category).length > 0 ? (
-                      getJokesByCategory(category).map((jok) => (
-                        <Square key={jok.id} borderWidth={1}>
-                          <Paragraph>{jok.joke}</Paragraph>
+                    {({ open }: { open: boolean }) => (
+                      <>
+                        <Paragraph>
+                          {index + 1}. {category}
+                        </Paragraph>
+                        <Square
+                          animation="quick"
+                          rotate={open ? "180deg" : "0deg"}
+                        >
+                          <ChevronDown size="$1" />
                         </Square>
-                      ))
-                    ) : (
-                      <Paragraph>Loading jokes...</Paragraph>
+                      </>
                     )}
-
-                    <Button
-                      onPress={() => {
-                        handleFetchMoreJokes(category);
-                      }}
+                  </Accordion.Trigger>
+                  <Accordion.HeightAnimator animation="medium">
+                    <Accordion.Content
+                      animation="medium"
+                      exitStyle={{ opacity: 0 }}
                     >
-                      <Text>Get more jokes</Text>
-                    </Button>
-                  </Accordion.Content>
-                </Accordion.HeightAnimator>
-              </Accordion.Item>
-            ))}
+                      {jokesList.length > 0 ? (
+                        jokesList.map((jok) => (
+                          <Square key={jok.id} borderWidth={1}>
+                            <Paragraph>{jok.joke}</Paragraph>
+                          </Square>
+                        ))
+                      ) : (
+                        <Paragraph>Loading jokes...</Paragraph>
+                      )}
+
+                      <Button
+                        onPress={() => {
+                          handleFetchMoreJokes(category);
+                        }}
+                      >
+                        <Text>Get more jokes</Text>
+                      </Button>
+                    </Accordion.Content>
+                  </Accordion.HeightAnimator>
+                </Accordion.Item>
+              );
+            })}
           </Accordion>
         )}
       </YStack>
